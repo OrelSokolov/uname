d8f1b88b3c0a8126f9e5cf1d1ab5d8ab9e06b5fb
Oleg Orlov <orelcokolov@gmail.com>
2019-09-08 12:12:13 +0300

    Add specs

--- spec/uname_spec.cr
+++ spec/uname_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
+require "spec2"
 
-describe Uname do
-  # TODO: Write tests
+Spec2.describe Uname do
+  it "machine" do
+    expect(Uname.machine.empty?).to be_false
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "release" do
+    expect(Uname.release.empty?).to be_false
+  end
+
+  it "version" do
+    expect(Uname.version.empty?).to be_false
+  end
+
+  it "sysname" do
+    expect(Uname.sysname.empty?).to be_false
+  end
+
+  it "nodename" do
+    expect(Uname.nodename.empty?).to be_false
   end
 end
